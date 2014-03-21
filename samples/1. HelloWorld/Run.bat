95922114a2529ae62dedaf61b7dd04d67a46d568
LiXizhi <lixizhi@yeah.net>
2014-03-21 19:41:34 +0800

    fix run with space and spacial letter

--- samples/1. HelloWorld/Run.bat
+++ samples/1. HelloWorld/Run.bat
@@ -1,2 +1,2 @@
 @echo off
-start '%CD%/../../redist/ParaEngineClient.exe single="false" mc="true" noupdate="true" dev="%CD%" bootstrapper="mods/HelloWorld/main.lua"'
+start ../../redist/ParaEngineClient.exe single="false" mc="true" noupdate="true" dev="%CD%" bootstrapper="mods/HelloWorld/main.lua"

0856d755dd9e5a53e91fd8e85963438ed83cd279
lipengg <lipengg@163.com>
2014-04-22 16:16:24 +0800

    [sync_src]sync_src.bat完成，自动同步src文件

--- src/sync_src.bat
+++ src/sync_src.bat
@@ -1,10 +1,21 @@
 echo off
-
-set default_sdk_path=D:\lxzsrc\ParaEngine\ParaWorld
-set /p SDK_PATH=请输入SDK路径.Enter键默认到%default_sdk_path%
-
-if %SDK_PATH% eq "" (
-	set SDK_PATH=%default_sdk_path%
+setlocal enabledelayedexpansion
+if exist %~dp0sdkpath.txt (
+	set /p default_sdk_path=<%~dp0sdkpath.txt
+) else (
+	set default_sdk_path=D:\workroot\KidsMovie\ParaEngineSDK	
 )
 
-xcopy %SDK_PATH%/src/*.*  .
+set /p SDK_PATH=请输入SDK路径(Enter键默认到%default_sdk_path%):
+
+if "%SDK_PATH%" == "" (
+	set SDK_PATH=%default_sdk_path%
+) else (
+	echo %SDK_PATH% >> %~dp0sdkpath.txt
+)
+
+xcopy "%SDK_PATH%\src\*.*"  "%~dp0" /e /d /y /c
+xcopy "%SDK_PATH%\config\Aries\creator\bom\*.xml"  "%~dp0config\Aries\creator\bom\" /e /d /y /c
+xcopy "%SDK_PATH%\config\Aries\creator\block_types.xml"  "%~dp0config\Aries\creator\" /e /d /y /c
+xcopy "%SDK_PATH%\config\Aries\creator\BuildingTasks.xml"  "%~dp0config\Aries\creator\" /e /d /y /c
+pause

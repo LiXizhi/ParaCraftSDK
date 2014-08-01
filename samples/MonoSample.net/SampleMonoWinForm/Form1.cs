217a83a9ea27ba6a3468e1c8c641d17c00dcb238
LiXizhi <lixizhi@yeah.net>
2014-08-01 11:47:54 +0800

    sample winform added

---
+++ samples/MonoSample.net/SampleMonoWinForm/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SampleMonoWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NPL.activate2("script/test/TestNPLMono.lua", "msg={uimsg='Hello World from WinForm'}");
+        }
+    }
+}
---
+++ samples/MonoSample.net/SampleMonoWinForm/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using NPLMono;
+
+namespace SampleMonoWinForm
+{
+    static class MainWindow
+    {
+        /// <summary>
+        /// -- Run WinForm in a separate UI thread:
+        /// NPL.CreateRuntimeState("ui", 0):Start();
+        /// NPL.activate("(ui)SampleMonoWinForm.dll/SampleMonoWinForm.MainWindow.cs", {});
+        /// -- Run WinForm in main UI thread:
+        /// NPL.activate("SampleMonoWinForm.dll/SampleMonoWinForm.MainWindow.cs", {});
+        /// </summary>
+        /// <param name="nType"></param>
+        /// <param name="npl_runtime_state"></param>
+        public static void activate(ref int nType, ref IntPtr npl_runtime_state)
+        {
+            // example 1: getting input message and write to application log
+            string msg = NPL.GetCurrentMsg(npl_runtime_state);
+            ParaGlobal.applog("This is from Mono C# files: " + msg);
+
+            Form1 myWindow = new Form1();
+            myWindow.ShowDialog();
+        }
+    }
+}

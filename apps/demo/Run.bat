400204ed4169fa7481b6a5d40b27c6a06cd733f3
LiXizhi <lixizhi@yeah.net>
2015-03-17 10:09:15 +0800

    removed app folder

--- apps/demo/Run.bat
+++
@@ -1,5 +0,0 @@
-@echo off 
-pushd "%~dp0../../redist/" 
-call "log.txt" 
-call "ParaEngineClient.exe" bootstrapper="source/demo/main.lua" single="false" mc="true" noupdate="true" dev="%~dp0"  
-popd 

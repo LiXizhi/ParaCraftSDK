d2a430d8a3cb57595250884dca47420c01ef4eb0
LiXizhi <lixizhi@yeah.net>
2016-10-18 19:21:49 +0800

    Demo item

--- _mod/Test/Mod/Test/DemoItem.lua
+++ _mod/Test/Mod/Test/DemoItem.lua
@@ -16,6 +16,28 @@ end
 
 function DemoItem:init()
 	LOG.std(nil, "info", "DemoItem", "init");
+	GameLogic.GetFilters().add_filter("block_types", function(xmlRoot) 
+		local blocks = commonlib.XPath.selectNode(xmlRoot, "/blocks/");
+		if(blocks) then
+			blocks[#blocks] = {name="block", attr={
+				id = 512, 
+				text = "Demo Item",
+				name = "DemoItem",
+				texture="Texture/blocks/bookshelf_three.png",
+				obstruction="true",
+				solid="true",
+				cubeMode="true",
+			}}
+		end
+		return xmlRoot;
+	end)
+
+	GameLogic.GetFilters().add_filter("block_list", function(xmlRoot) 
+		for node in commonlib.XPath.eachNode(xmlRoot, "/blocklist/category") do
+			
+		end
+		return xmlRoot;
+	end)
 end
 
 function DemoItem:OnWorldLoad()
